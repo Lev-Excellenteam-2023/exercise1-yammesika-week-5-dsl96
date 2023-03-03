a95291fe2829828d2bdeae3c85c67cf35e3b982c
dov liberman <doliberm@g.jct.ac.il>
2023-03-03 13:56:24 +0200

    ex1

---
+++ 6.5.group_by.py
@@ -0,0 +1,14 @@
+# group_by 6.5.1
+from collections import defaultdict
+
+
+def group_by(func, lst):
+    d = defaultdict(list)
+
+    for item in lst:
+        d[func(item)].append(item)
+
+    return dict(d)
+
+a = group_by(len, ["23","hjk","23","hjk","l"])
+print (a)
